1f7ace9b14986f595a31dda64cd4541e8ac254d3
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-04-17 16:04:12 +0200

    user details service

--- src/main/java/ch/bbw/m183/vulnerapp/BasicConfiguration.java
+++ src/main/java/ch/bbw/m183/vulnerapp/BasicConfiguration.java
@@ -1,27 +1,33 @@
 package ch.bbw.m183.vulnerapp;
 
+import ch.bbw.m183.vulnerapp.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 public class BasicConfiguration {
 
     @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return (username) -> userRepository.findById(username)
+                .map(entity -> new User(entity.getUsername(), entity.getPassword(), Collections.emptyList()))
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
         return http.build();
     }
 
